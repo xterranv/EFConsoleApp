b13c707fd1085df2f9d54513aa2d01c62daf94ce
xterranv <BeachsideTech@yahoo.com>
2020-12-13 20:38:32 -0500

    First commit

---
+++ EFConsoleApp/EFConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var cntxt = new SchoolContext())
+            {
+                var student = new Student() { StudentName = "Adam" };
+
+                cntxt.Students.Add(student);
+                cntxt.SaveChanges();
+
+                Console.Write("Student entry saved successfully.");                
+            }
+        }
+    }
+}
